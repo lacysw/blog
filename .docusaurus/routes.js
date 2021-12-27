b2567fbb053b761af4261259572a3dfe5433f468
lacysw <accounts@swlacy.com>
2021-12-27 03:43:08 -0800

    upd

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -45,7 +45,7 @@ export default [
   },
   {
     path: '/docs',
-    component: ComponentCreator('/docs','ea9'),
+    component: ComponentCreator('/docs','b1b'),
     routes: [
       {
         path: '/docs',
@@ -54,44 +54,44 @@ export default [
         'sidebar': "tutorialSidebar"
       },
       {
+        path: '/docs/extended-tutorials/how-i-made-this-website/bonus',
+        component: ComponentCreator('/docs/extended-tutorials/how-i-made-this-website/bonus','a1a'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/docs/extended-tutorials/how-i-made-this-website/deploy-to-firebase',
+        component: ComponentCreator('/docs/extended-tutorials/how-i-made-this-website/deploy-to-firebase','902'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/docs/extended-tutorials/how-i-made-this-website/dev-env',
+        component: ComponentCreator('/docs/extended-tutorials/how-i-made-this-website/dev-env','d09'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/docs/extended-tutorials/how-i-made-this-website/edit-defaults',
+        component: ComponentCreator('/docs/extended-tutorials/how-i-made-this-website/edit-defaults','6fb'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/docs/extended-tutorials/how-i-made-this-website/introduction',
+        component: ComponentCreator('/docs/extended-tutorials/how-i-made-this-website/introduction','0ca'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/docs/extended-tutorials/how-i-made-this-website/register-domain',
+        component: ComponentCreator('/docs/extended-tutorials/how-i-made-this-website/register-domain','355'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
         path: '/docs/reports/previse',
         component: ComponentCreator('/docs/reports/previse','5cb'),
-        exact: true,
-        'sidebar': "tutorialSidebar"
-      },
-      {
-        path: '/docs/website-hosting/bonus',
-        component: ComponentCreator('/docs/website-hosting/bonus','761'),
-        exact: true,
-        'sidebar': "tutorialSidebar"
-      },
-      {
-        path: '/docs/website-hosting/deploy-to-firebase',
-        component: ComponentCreator('/docs/website-hosting/deploy-to-firebase','2ce'),
-        exact: true,
-        'sidebar': "tutorialSidebar"
-      },
-      {
-        path: '/docs/website-hosting/dev-env',
-        component: ComponentCreator('/docs/website-hosting/dev-env','6c5'),
-        exact: true,
-        'sidebar': "tutorialSidebar"
-      },
-      {
-        path: '/docs/website-hosting/edit-defaults',
-        component: ComponentCreator('/docs/website-hosting/edit-defaults','452'),
-        exact: true,
-        'sidebar': "tutorialSidebar"
-      },
-      {
-        path: '/docs/website-hosting/introduction',
-        component: ComponentCreator('/docs/website-hosting/introduction','3ad'),
-        exact: true,
-        'sidebar': "tutorialSidebar"
-      },
-      {
-        path: '/docs/website-hosting/register-domain',
-        component: ComponentCreator('/docs/website-hosting/register-domain','c0e'),
         exact: true,
         'sidebar': "tutorialSidebar"
       }
--- docs/welcome.md
+++ docs/welcome.md
@@ -4,8 +4,8 @@ slug: /
 ---
 
 # Welcome!
-The *Docs & Tutorials* section hosts a growing number of technical articles, mostly focused on infosec challenges and Linux-related development projects. While relatively sparse now, my intention is to share my interesting activity here, so if nothing immediately sparks your interest, please check back soon. The sidebar on the left can be used to navigate between the pages of this section.
+The *Docs & Tutorials* section hosts a growing number of technical articles, focused primarily on cybersecurity challenges (HackTheBox, TryHackMe, etc.) and miscellaneous development projects. While relatively sparse now, I intend to share much of my interesting activity here. If nothing immediately sparks your interest, please check back soon. The left-hand sidebar can be used to navigate between the pages of this section.
 
-If you have any suggestions or criticisms related to my content, [I can be reached over email](mailto:contact@swlacy.com). I aim to provide accessible and accurate material, so I take all feedback seriously.
+Suggestions or criticisms? [I can be reached over email](mailto:contact@swlacy.com). I aim to provide accessible and accurate material, so I take all feedback seriously.
 
 Enjoy!
